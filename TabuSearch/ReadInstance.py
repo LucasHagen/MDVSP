2604814634928b7fcc54d0f322b862a5c8e88fff
Leonardo Bombardelli <leonardo.b.bombardelli@gmail.com>
2018-12-01 15:59:38 -0200

    Implemented parser to get info

---
+++ TabuSearch/ReadInstance.py
@@ -0,0 +1,27 @@
+
+def ReadInstance(fileName):
+    with open(fileName, "r") as fileInstance:
+        rawText = fileInstance.readlines()
+
+    # First line: d = number of depots, t = number of trips, ld = d values, maxbus
+    firstLine = [int(x) for x in rawText[0].split("\t")]
+    nDepots = firstLine[0]
+    nTrips = firstLine[1]
+    maxBus = firstLine[2:]
+    rawText.pop(0)
+
+    depotToTrip = {}
+    for d in range(nDepots):
+        depotToTrip[d] = [ x for x in rawText[0].split("\t")[nDepots:-1] ] #-1 defines that we will remove the \n at the end of every list
+        rawText.pop(0)
+    
+    tripToDepot = {}
+    tripToTrip = {}
+    for t in range(nTrips):
+        tripToDepot[t] = [ x for x in rawText[0].split("\t")[:nDepots] ]
+        tripToTrip[t] = [ x for x in rawText[0].split("\t")[nDepots:-1] ]
+        rawText.pop(0)
+    if(rawText == []):
+        raise Exception("Not a valid instance: first line probably has wrong info.")
+    return()
+
---
+++ TabuSearch/Test.py
@@ -0,0 +1,4 @@
+import TS
+from ReadInstance import ReadInstance
+
+ReadInstance("./TabuSearch/instances-inp/m4n500s2.inp")
