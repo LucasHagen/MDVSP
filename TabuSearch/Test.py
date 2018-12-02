5d13b379d37a53787ffb6760960cb959eb13707d
Leonardo Bombardelli <leonardo.b.bombardelli@gmail.com>
2018-12-02 20:30:05 -0200

    Continued modelling the problem, but we have some issues

--- TabuSearch/Test.py
+++ TabuSearch/Test.py
@@ -1,4 +1,5 @@
 import TS
 from ReadInstance import ReadInstance
+from ModellingProblem import Solution
 
 nDepots, nTrips, maxBus, depotToTrip, tripToDepot, tripToTrip = ReadInstance("./TabuSearch/instances-inp/m4n500s2.inp")
