b8cf1c2de62c120e64d3e49c374e3848962d5267
Leonardo Bombardelli <lbombardelli@w03266798.cipucrs.pucrs.br>
2018-12-03 15:59:45 -0200

    Trying to model the constructive solution

--- TabuSearch/ModellingProblem.py
+++ TabuSearch/ModellingProblem.py
@@ -1,47 +1,77 @@
 import numpy as np
+import random
+
+class ListBus():
+    def __init__(self, firstTrip: int):
+        self.depot = -1 #Not decided yet
+        self.path = [firstTrip]
+    
+    def insertNewTrip(self, trip: int):
+        self.path.append(trip)
+    
+    def insertNewDepot(self, depot: int):
+        self.depot = depot
 
 class Solution():
-    def __init__(self, nDepots: int, nTrips: int, maxBus: list):
+    def __init__(self, nDepots: int, nTrips: int, maxBus: list, depotToTrip: np.ndarray, tripToDepot: np.ndarray, tripToTrip: np.ndarray):
         self.nDepots = nDepots
         self.nTrips = nTrips
         self.maxBus = maxBus
+        
+        self.depotToTrip = depotToTrip
+        self.tripToTrip = tripToTrip
+        self.tripToDepot = tripToDepot
+
         self.solutionDepots = np.zeros(shape = (nDepots, nTrips), dtype = int)  #Represents if a bus is going out of the depot to a trip. Initially has only zeros, and has binary values
         self.solutionTrips = np.zeros(shape = (nTrips, nTrips), dtype = int)    #Represents if a bus is going from a trip to another one.
-        self.value = -1 #Minus one means that the actual value has not been calculated
+        
+        self.value = 0 
+        
+        self.listOfTripsPerBus = []
         
 
-    def generateRandomSolution(self, seedSolution):
-        pass
-    
-    def checkRestictionsAndValue(self, depotToTrip, tripToDepot, tripToTrip):
-        """Returns if a problem is in conformity with it's restrictions, and refresh the value of the value function"""
-        self.value = 0
-        
-        #Restricion: We must pass through every trip
-        listOFTrips = np.zeros(shape=self.nTrips, dtype=int)    #We start filling it with zeros, and we'll fill it with the amount of ocourrences in it as we progress through the tables
+    def generateConstructiveSolution(self, seedSolution):
+        random.seed(seedSolution)
 
-        # We must respect the limit of bus in each depot
-        for d in range(self.nDepots):
-            actualBusCount = 0
+        candidates = [n for n in range(self.nTrips)] #Candidates = list of trips that have not been visited yet by any bus!
+
+        while(candidates != [])
+            nextCandidate = candidates.pop(random.randint(0, len(candidates)))  #We get a random candidate from the list
+            self.listOfTripsPerBus.append(ListBus(nextCandidate))
             
-            for t in range(self.nTrips):
-                if (self.solutionDepots[d][t] > 0):
-                    actualBusCount+=1
-                    self.value+=depotToTrip[d][t]
-                    else:
-                        return False
-                    listOFTrips[t] += 1
+            
+                costOfTripsFromActual = tripToTrip[actualTrip]
+                reachableTrips = [n for n in costOfTripsFromActual if n > 0]
+                minValue = -1
 
-            if (actualBusCount > self.maxBus[d]):
-                return False
-        
-        # We need to check where each trip is going
-        for t1 in range(self.nTrips):
-            #We need to go through the solutionTruos and check the trips, problem is, we also need to keep track of the graph because the
-            #bus that's going needs to go back to the depot, and i don't know how to make that efficiently
-            #Also, we need to make sure only ONE bus goes through each trip, and only one bus leaves from it
-            #Since it will be impossible to keep track of where the bus will need to go back
-            pass
+                while(reachableTrips != [] or minValue != -1):
+                    if reachableTrips.index(min(reachableTrips)) in candidates:
+                        minValue = min(reachableTrips)
+                
+                
+                
+                
+
+
+
+
+            
+            
+
+        #Now, we need to check all the trips that we can get going from nextCandidate:
+    
+
+    def whichTripCanGo(self, actualTrip: int, candidates):
+        """
+        Returns the trip we will go next and the value we will increment in it
+        Returns: (nextTrip: int, valueToIncrease: int)
+        """
+        costOfTripsFromActual = tripToTrip[actualTrip]
+
+
+
+    
+
 
 
         
